87385c7cf626b826f42a71b1fe04ce56b6daa55e
20191230036 <kahe.estrela@academico.ifpb.edu.br>
2024-09-09 13:50:54 -0300

    Exceçao por arquivo

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import java.io.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+
+
         try{
             Pessoa pessoa = new Pessoa("111.111-01","joao");
 
@@ -11,4 +15,15 @@ public class Main {
             JOptionPane.showMessageDialog(null,ex.getMessage());
         }
     }
+    private void escreverNoArquivo(String file, String mensagem) throws IOException {
+
+        File file1 = new  File(file);
+        file1.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file1));
+        writer.write(mensagem);
+        writer.newLine();
+        writer.close();
+
+    }
+
 }
